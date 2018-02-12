8209332389a7371eb4ade4544cb3cfc3ab9e77d9
Nikolaou Stefanos <snikolaou@windowslive.com>
2018-02-13 01:48:47 +0200

    9th chapter is completer

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+from rango.models import UserProfile
 # Register your models here.
 
 
@@ -16,3 +17,6 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
+
+
+admin.site.register(UserProfile)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -111,6 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+'django.contrib.auth.hashers.BCryptPasswordHasher',
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -133,3 +139,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_URL = '/rango/login/'
