54f18051c58480cbf654099b49d101f7749d118f
Nikolaou Stefanos <snikolaou@windowslive.com>
2018-02-13 06:30:45 +0200

    10th chapter is completed

--- rango/views.py
+++ rango/views.py
@@ -12,38 +12,27 @@ from django.core.urlresolvers import reverse
 
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
+
 def index(request):
-    #return HttpResponse("Rango says hey there partner! <br /> <a href='./about'>click here </a>")
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-#    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-#    return render(request, 'rango/index.html', context=context_dict)
-
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary
-    # that will be passed to the template engine.
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
-
-    top_5_pages = Page.objects.order_by('-views')[:5]
-
-
-
-
-    context_dict = {'categories': category_list , 'pages' : top_5_pages}
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
+    visitor_cookie_handler(request)
     #return HttpResponse("Rango says here is the about page. <br /> <a href='/rango/'>click here </a>")
-
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_dict = {'myName': "Nikolaou Stefanos" , "imageName" : "cat.jpg"}
+    context_dict['visits'] = request.session['visits']
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -219,3 +208,26 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return HttpResponseRedirect(reverse('index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -142,3 +142,5 @@ STATIC_URL = '/static/'
 
 
 LOGIN_URL = '/rango/login/'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
